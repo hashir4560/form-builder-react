088b31fecbb74734083e5109edf06affafc95159
hashir4560 <haashirraja@gmail.com>
2023-09-20 00:21:31 +0500

    Form Builder Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import './App.css';
-import FormBuilder from './components/FormBuilder';
-import DropContainer from './components/DropContainer';
+import React from "react";
+import "./App.css";
+import FormBuilder from "./components/FormBuilder";
 
 function App() {
   return (
     <div className="App">
       <FormBuilder />
-      
     </div>
   );
 }
--- src/components/DropContainer.js
+++ src/components/DropContainer.js
@@ -1,39 +1,51 @@
-import React from 'react';
+import React from "react";
 
 const DropContainer = ({ formFields, handleDrop, handleRemoveField }) => {
   return (
-    <div className="form-container" onDragOver={(event) => event.preventDefault()} onDrop={handleDrop}>
+    <div className="form-builder-container">
       <h2>Form Builder</h2>
-      <form>
-        {formFields.map((field, index) => (
-          <div key={index} className="form-field">
-            {field === 'Name' ? (
-              <div>
-                <label>Name:</label>
-                <input type="text" placeholder="Name" />
-              </div>
-            ) : field === 'Input' ? (
-              <div>
-                <label>Input:</label>
-                <input type="text" placeholder="Input field" />
-              </div>
-            ) : field === 'Checkbox' ? (
-              <div>
-                <label>
-                  <input type="checkbox" />
-                  Checkbox
-                </label>
-              </div>
-            ) : field === 'Date' ? (
-              <div>
-                <label>Date:</label>
-                <input type="date" />
-              </div>
-            ) : null}
-            <button onClick={() => handleRemoveField(index)}>Remove</button>
-          </div>
-        ))}
-      </form>
+      <div
+        className="form-builder"
+        onDragOver={(event) => event.preventDefault()}
+        onDrop={handleDrop}
+      >
+        <div className="form-fields-wrapper">
+          {formFields.map((field, index) => (
+            <div key={index} className="form-field">
+              {field === "Name" ? (
+                <div>
+                  <label>Name:</label>
+                  <input type="text" placeholder="Name" />
+                </div>
+              ) : field === "Input" ? (
+                <div>
+                  <label>Input:</label>
+                  <input type="text" placeholder="Input field" />
+                </div>
+              ) : field === "Checkbox" ? (
+                <div>
+                  <label>
+                    <input type="checkbox" />
+                    Checkbox
+                  </label>
+                </div>
+              ) : field === "Date" ? (
+                <div>
+                  <label>Date:</label>
+                  <input type="date" />
+                </div>
+              ) : null}
+              <button onClick={() => handleRemoveField(index)}>Remove</button>
+            </div>
+          ))}
+        </div>
+
+        <div className="submit-button-container">
+          <button className="submit-button" type="submit">
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
